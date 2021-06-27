ddf889bfe796fbbccd9968e27abcd58a4514580f
ganeshalapati <alapatiganesh31@gmail.com>
2021-06-27 22:32:20 +0530

    Added tictactoe board

---
+++ TicTacToe.java
@@ -0,0 +1,19 @@
+
+public class TicTacToe {
+	 public static char board[];
+	    public static char[] printMatrix()
+	    {
+	        board=new char[10];
+	        for(int i=1;i<board.length;i++)
+	        {
+	            board[i]=' ';
+	        }
+	        return board;
+	    }
+
+	    public static void main(String[] args)
+	    {
+	        System.out.println("Welcome");
+	        char board[]=printMatrix();
+
+}}
